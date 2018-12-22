fc2a4ad8d45d56e971b6c14b5c8090ed2f78f86b
Robert Zehnder <robert@kisdigital.com>
2018-12-21 19:59:01 -0500

    stuff

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
-const chalk = require('chalk')
+const chalk = require('chalk');
 
-const app = express()
+const app = express();
 
-const apiController = require('./controller/api')
+const apiController = require('./controller/api');
 
-app.use(cors())
+app.use(cors());
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/api/test', apiController.test)
+app.get('/api/test', apiController.test);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), process.env.PORT || 3000, app.get('env'))
-    console.log('  Press CTRL-C to stop\n')
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), process.env.PORT || 3000, app.get('env'));
+    console.log('  Press CTRL-C to stop\n');
 });
